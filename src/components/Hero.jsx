09ed2a5cc8f952e82a3253ce34ca7680cfe8c20b
akkerii <saifallah.chaffarakkeri@esprit.tn>
2025-02-05 22:20:37 +0100

    Fix mobile responsiveness for Hero section and 3D model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section className="relative w-full h-screen mx-auto">
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
@@ -26,8 +26,6 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
-
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
@@ -44,6 +42,10 @@ const Hero = () => {
             />
           </div>
         </a>
+      </div>
+
+      <div className="absolute xs:bottom-0 bottom-0 w-full h-[calc(100vh-200px)]">
+        <ComputersCanvas />
       </div>
     </section>
   );
